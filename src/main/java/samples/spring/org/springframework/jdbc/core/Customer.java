96cc68e750abf06418a83e468de8d5264885617e
izeye <izeye@naver.com>
2014-12-01 07:51:14 +0900

    Add a JdbcTemplate sample from http://spring.io/guides/gs/relational-data-access/.

--- src/main/java/samples/spring/org/springframework/jdbc/core/Customer.java
+++ src/main/java/samples/spring/org/springframework/jdbc/core/Customer.java
@@ -4,4 +4,48 @@ package samples.spring.org.springframework.jdbc.core;
  * Created by izeye on 14. 11. 30..
  */
 public class Customer {
+
+    private long id;
+    private String firstName;
+    private String lastName;
+
+    public Customer(long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
 }
--- src/main/java/samples/spring/org/springframework/jdbc/core/JdbcTemplateTest.java
+++ src/main/java/samples/spring/org/springframework/jdbc/core/JdbcTemplateTest.java
@@ -1,7 +1,90 @@
 package samples.spring.org.springframework.jdbc.core;
 
+import org.junit.Ignore;
+import org.junit.Test;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 /**
  * Created by izeye on 14. 11. 30..
  */
 public class JdbcTemplateTest {
+
+    @Test
+    public void testJdbc_H2_Mem() {
+        testJdbcTemplate("jdbc:h2:mem");
+    }
+
+    @Test
+    @Ignore
+    public void testJdbc_H2_Mem_() {
+        testJdbcTemplate("jdbc:h2:mem:");
+    }
+
+    @Test
+    public void testJdbc_H2_Mem_Test() {
+        testJdbcTemplate("jdbc:h2:mem:test");
+    }
+
+    @Test
+    public void testJdbc_H2_Mem_Test_DB_CLOSE_DELAY() {
+        testJdbcTemplate("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1");
+    }
+
+    @Test
+    public void testJdbc_H2_Test() {
+        testJdbcTemplate("jdbc:h2:~/test");
+    }
+
+    private void testJdbcTemplate(String url) {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setDriverClass(org.h2.Driver.class);
+        dataSource.setUsername("sa");
+        dataSource.setUrl(url);
+        dataSource.setPassword("");
+
+        testJdbcTemplate(dataSource);
+    }
+
+    private void testJdbcTemplate(SimpleDriverDataSource dataSource) {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+
+        System.out.println("Creating tables");
+        jdbcTemplate.execute("drop table customers if exists");
+        jdbcTemplate.execute(
+                "create table customers (id serial, first_name varchar(255), last_name varchar(255))");
+
+        String[] names = "John Woo;Jeff Dean;Josh Bloch;Josh Long".split(";");
+        for (String fullname : names) {
+            String[] name = fullname.split(" ");
+            System.out.printf("Inserting customer record for %s %s\n", name[0], name[1]);
+            jdbcTemplate.update(
+                    "INSERT INTO customers (first_name, last_name) VALUES (?, ?)", name[0], name[1]);
+        }
+
+        System.out.println("Querying for customer records where first_name = 'Josh':");
+        List<Customer> results = jdbcTemplate.query(
+                "select * from customers where first_name = ?",
+                new Object[] { "Josh" },
+                new RowMapper<Customer>() {
+                    @Override
+                    public Customer mapRow(ResultSet rs, int rowNum) throws SQLException {
+                        return new Customer(
+                                rs.getLong("id"),
+                                rs.getString("first_name"),
+                                rs.getString("last_name"));
+                    }
+                }
+        );
+
+        for (Customer customer : results) {
+            System.out.println(customer);
+        }
+    }
+
 }
